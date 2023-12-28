d35b0524e1696014011bb97a32b7626313f7eec6
mohamed abd elkhalk <01147018091.m@gmial.com>
2023-12-28 20:37:02 +0200

    a

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <label htmlFor="">email</label>
+        <input type="email" name="" id="" />
+        <label htmlFor="">password</label>
+        <input type="password" name="" id="" />
+        <p>forget password</p>
+        <p>
+          don't have account try to <a href="">signup</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
